5b29bb0af0375c809ce806d8ac04daab9a4379db
sergei urevich <daray2513251325@gmail.com>
2023-05-17 02:05:47 +0300

    added sliders

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // активация слайдера
+    new ItcSimpleSlider('.itcss', {
+      loop: true,
+      autoplay: true,
+      swipe: true
+    });
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    // активация слайдера
+    new ItcSimpleSlider('.itcss2', {
+      loop: true,
+      autoplay: true,
+      swipe: true
+    });
+  });
+
