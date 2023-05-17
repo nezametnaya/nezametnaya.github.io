547ffe18fd5125de0ca61ee47945a5769c49fd71
sergei urevich <daray2513251325@gmail.com>
2023-05-17 08:56:40 +0300

    Fixed all sliders

--- js/index.js
+++ js/index.js
@@ -8,7 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   document.addEventListener('DOMContentLoaded', () => {
     // активация слайдера
-    new ItcSimpleSlider('.itcss2', {
+    new ItcSimpleSlider('.itcss2-1', {
+      loop: true,
+      autoplay: true,
+      swipe: true
+    });
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    // активация слайдера
+    new ItcSimpleSlider('.itcss2-2', {
       loop: true,
       autoplay: true,
       swipe: true
---
+++ js/individual.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // активация слайдера
+    new ItcSimpleSlider('.itcss2-1', {
+      loop: true,
+      autoplay: true,
+      swipe: true
+    });
+  });
